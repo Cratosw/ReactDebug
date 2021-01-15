ba3632a2653c0e512144a885e87ad0f02c581bad
苟文博 <wenbo248281051@outlook.com>
2021-01-15 16:03:35 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  debugger
+  const [count, setCount] = React.useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
         <a
           className="App-link"
